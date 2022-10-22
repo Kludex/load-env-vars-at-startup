d14e0d826a0eae437136af42dadf9e8739a39418
Marcelo Trylesinski <marcelotryle@gmail.com>
2022-10-22 22:47:25 +0200

    Remove app test folder

--- app/config.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseSettings
-
-
-class Settings(BaseSettings):
-    ENV_VAR: str
-
-
-settings = Settings()
--- app/main.py
+++
@@ -1,4 +0,0 @@
-from os import environ, getenv
-
-environ.get("ENV_VAR")
-getenv("ENV_VAR")
